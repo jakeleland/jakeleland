84c64f7393ce8eae222355701b7d312c6cc902f2
jakeleland <60122756+jakeleland@users.noreply.github.com>
2020-02-03 21:53:57 -0600

    Add files via upload

---
+++ IT354/script.js
@@ -0,0 +1,15 @@
+function number() {
+	alert(typeof 42);
+}
+
+function string() {
+	alert(typeof "hello");
+}
+
+function boolean() {
+	alert(typeof true);
+}
+
+function undefinedf() {
+	alert(typeof hello);
+}
